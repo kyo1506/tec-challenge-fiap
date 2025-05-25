6c3b007abdcc653ce516f0b1bd1828928f704c6e
Willian Nalbert <williannalbert@hotmail.com>
2025-05-24 21:07:19 -0300

    finalizaçã dos tests de promotion

--- src/TecChallenge.Tests/PromotionServiceTest.cs
+++ src/TecChallenge.Tests/PromotionServiceTest.cs
@@ -354,6 +354,531 @@ public class PromotionServiceTest
         _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
     }
 
+    [Fact]
+    public async Task DeletePromotion_Success()
+    {
+        var id = Guid.NewGuid();
 
-    
+        var promotion = new Promotion
+        {
+            Name = "Promotion Test",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(10),
+            GamesOnSale = []
+        };
+
+        _promotionRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<Promotion, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<Promotion, object>>[]>()
+            ))
+            .ReturnsAsync(promotion);
+
+        _unitOfWorkMock
+            .Setup(u => u.CommitAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var result = await _promotionService.DeleteAsync(id);
+
+        result.Should().BeTrue();
+
+        _promotionRepositoryMock.Verify(p => p.Delete(It.IsAny<Promotion>()), Times.Once);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeletePromotion_NotFound()
+    {
+        var id = Guid.NewGuid();
+
+        _promotionRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<Promotion, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<Promotion, object>>[]>()
+            ))
+            .ReturnsAsync((Promotion)null);
+
+        var result = await _promotionService.DeleteAsync(id);
+
+        result.Should().BeNull();
+
+        _promotionRepositoryMock.Verify(p => p.Delete(It.IsAny<Promotion>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+    [Fact]
+    public async Task DeletePromotion_WithGamesOnSale_CannotDelete()
+    {
+        var id = Guid.NewGuid();
+
+        var promotion = new Promotion
+        {
+            Name = "Promotion Test",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(10),
+            GamesOnSale = [new PromotionGame()]
+        };
+
+        _promotionRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<Promotion, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<Promotion, object>>[]>()
+            ))
+            .ReturnsAsync(promotion);
+
+        var result = await _promotionService.DeleteAsync(id);
+
+        result.Should().BeFalse();
+
+        _promotionRepositoryMock.Verify(p => p.Delete(It.IsAny<Promotion>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task DeletePromotion_CommitThrowsException()
+    {
+        var id = Guid.NewGuid();
+
+        var promotion = new Promotion
+        {
+            Name = "Promotion Test",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(10),
+            GamesOnSale = []
+        };
+
+        _promotionRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<Promotion, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<Promotion, object>>[]>()
+            ))
+            .ReturnsAsync(promotion);
+
+        _promotionRepositoryMock
+            .Setup(p => p.Delete(It.IsAny<Promotion>()));
+
+        _unitOfWorkMock
+            .Setup(u => u.CommitAsync(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Commit exception"));
+
+        _unitOfWorkMock
+            .Setup(u => u.RollbackAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        Func<Task> act = async () => await _promotionService.DeleteAsync(id);
+
+        await act.Should().ThrowAsync<Exception>().WithMessage("Commit exception");
+
+        _unitOfWorkMock.Verify(u => u.RollbackAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task AddGamesOnSale_PromotionNotFound_ReturnsNull()
+    {
+        var id = Guid.NewGuid();
+
+        var model = new PromotionGame();
+        model.PromotionId = Guid.NewGuid();
+        model.GameId = Guid.NewGuid();
+        model.DiscountPercentage = 20;
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(It.IsAny<Expression<Func<PromotionGame, bool>>>(), true))
+            .ReturnsAsync((PromotionGame)null);
+
+        var result = await _promotionService.UpdatePromotionGameAsync(id, model);
+
+        result.Should().BeNull();
+
+        _promotionGameRepositoryMock.Verify(p => p.Update(It.IsAny<PromotionGame>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task AddGamesOnSale_AllGamesAlreadyExist_ReturnsFalse()
+    {
+        var id = Guid.NewGuid();
+        var gameId = Guid.NewGuid();
+
+        var existingGame = new PromotionGame { GameId = gameId };
+
+        var promotion = new Promotion
+        {
+            Name = "Promotion Test",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(10),
+            GamesOnSale = [existingGame]
+        };
+
+        var gamesOnSale = new List<PromotionGame> { existingGame };
+
+        _promotionRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<Promotion, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<Promotion, object>>[]>()
+            ))
+            .ReturnsAsync(promotion);
+
+        var result = await _promotionService.AddGamesOnSaleAsync(id, gamesOnSale);
+
+        result.Should().BeFalse();
+
+        _promotionRepositoryMock.Verify(p => p.Update(It.IsAny<Promotion>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task AddGamesOnSale_GameInOtherPromotion_ReturnsFalse()
+    {
+        var id = Guid.NewGuid();
+        var gameId = Guid.NewGuid();
+
+        var promotion = new Promotion
+        {
+            Name = "Promotion Test",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(10),
+            GamesOnSale = []
+        };
+
+        var newGame = new PromotionGame { GameId = gameId };
+        var gamesOnSale = new List<PromotionGame> { newGame };
+
+        _promotionRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<Promotion, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<Promotion, object>>[]>()
+            ))
+            .ReturnsAsync(promotion);
+
+        _promotionGameRepositoryMock
+            .Setup(pg => pg.AnyAsync(
+                It.IsAny<Expression<Func<PromotionGame, bool>>>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(true);
+
+        var result = await _promotionService.AddGamesOnSaleAsync(id, gamesOnSale);
+
+        result.Should().BeFalse();
+
+        _promotionRepositoryMock.Verify(p => p.Update(It.IsAny<Promotion>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task AddGamesOnSale_Success_ReturnsTrue()
+    {
+        var id = Guid.NewGuid();
+        var gameId = Guid.NewGuid();
+
+        var promotion = new Promotion
+        {
+            Name = "Promotion Test",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(10),
+            GamesOnSale = []
+        };
+
+        var newGame = new PromotionGame { GameId = gameId };
+        var gamesOnSale = new List<PromotionGame> { newGame };
+
+        _promotionRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<Promotion, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<Promotion, object>>[]>()
+            ))
+            .ReturnsAsync(promotion);
+
+        _promotionGameRepositoryMock
+            .Setup(pg => pg.AnyAsync(
+                It.IsAny<Expression<Func<PromotionGame, bool>>>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(false);
+
+        _unitOfWorkMock
+            .Setup(u => u.CommitAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var result = await _promotionService.AddGamesOnSaleAsync(id, gamesOnSale);
+
+        result.Should().BeTrue();
+
+        _promotionRepositoryMock.Verify(p => p.Update(It.Is<Promotion>(x => x.GamesOnSale.Contains(newGame))), Times.Once);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task AddGamesOnSale_CommitThrowsException()
+    {
+        var id = Guid.NewGuid();
+        var gameId = Guid.NewGuid();
+
+        var promotion = new Promotion
+        {
+            Name = "Promotion Test",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(10),
+            GamesOnSale = []
+        };
+
+        var newGame = new PromotionGame { GameId = gameId };
+        var gamesOnSale = new List<PromotionGame> { newGame };
+
+        _promotionRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<Promotion, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<Promotion, object>>[]>()
+            ))
+            .ReturnsAsync(promotion);
+
+        _promotionGameRepositoryMock
+            .Setup(pg => pg.AnyAsync(
+                It.IsAny<Expression<Func<PromotionGame, bool>>>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(false);
+
+        _promotionRepositoryMock
+            .Setup(p => p.Update(It.IsAny<Promotion>()));
+
+        _unitOfWorkMock
+            .Setup(u => u.CommitAsync(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Commit exception"));
+
+        _unitOfWorkMock
+            .Setup(u => u.RollbackAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        Func<Task> act = async () => await _promotionService.AddGamesOnSaleAsync(id, gamesOnSale);
+
+        await act.Should().ThrowAsync<Exception>().WithMessage("Commit exception");
+
+        _unitOfWorkMock.Verify(u => u.RollbackAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+    [Fact]
+    public async Task UpdatePromotionGame_PromotionGameNotFound()
+    {
+        var id = Guid.NewGuid();
+
+        var model = new PromotionGame();
+        model.PromotionId = Guid.NewGuid();
+        model.GameId = Guid.NewGuid();
+        model.DiscountPercentage = 20;
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(It.IsAny<Expression<Func<PromotionGame, bool>>>(), true))
+            .ReturnsAsync((PromotionGame)null);
+
+        var result = await _promotionService.UpdatePromotionGameAsync(id, model);
+
+        result.Should().BeNull();
+
+        _promotionGameRepositoryMock.Verify(p => p.Update(It.IsAny<PromotionGame>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task UpdatePromotionGame_PromotionOrGameChanged_()
+    {
+        var id = Guid.NewGuid();
+
+        var existing = new PromotionGame();
+        existing.PromotionId = Guid.NewGuid();
+        existing.GameId = Guid.NewGuid();
+        existing.DiscountPercentage = 10;
+
+        var model = new PromotionGame();
+        model.PromotionId = Guid.NewGuid();
+        model.GameId = existing.GameId;
+        model.DiscountPercentage = 20;
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(It.IsAny<Expression<Func<PromotionGame, bool>>>(), true))
+            .ReturnsAsync(existing);
+
+        var result = await _promotionService.UpdatePromotionGameAsync(id, model);
+
+        result.Should().BeNull();
+
+        _promotionGameRepositoryMock.Verify(p => p.Update(It.IsAny<PromotionGame>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task UpdatePromotionGame_Success()
+    {
+        var id = Guid.NewGuid();
+        var promotionId = Guid.NewGuid();
+        var gameId = Guid.NewGuid();
+
+        var existing = new PromotionGame();
+        existing.PromotionId = promotionId;
+        existing.GameId = gameId;
+        existing.DiscountPercentage = 10;
+
+        var model = new PromotionGame();
+        model.PromotionId = promotionId;
+        model.GameId = gameId;
+        model.DiscountPercentage = 25;
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(It.IsAny<Expression<Func<PromotionGame, bool>>>(), true))
+            .ReturnsAsync(existing);
+
+        _unitOfWorkMock
+            .Setup(u => u.CommitAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var result = await _promotionService.UpdatePromotionGameAsync(id, model);
+
+        result.Should().BeTrue();
+        existing.DiscountPercentage.Should().Be(model.DiscountPercentage);
+
+        _promotionGameRepositoryMock.Verify(p => p.Update(It.Is<PromotionGame>(pg => pg.DiscountPercentage == model.DiscountPercentage)), Times.Once);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdatePromotionGame_CommitThrowsException()
+    {
+        var id = Guid.NewGuid();
+        var promotionId = Guid.NewGuid();
+        var gameId = Guid.NewGuid();
+
+        var existing = new PromotionGame();
+        existing.PromotionId = promotionId;
+        existing.GameId = gameId;
+        existing.DiscountPercentage = 10;
+
+        var model = new PromotionGame();
+        model.PromotionId = promotionId;
+        model.GameId = gameId;
+        model.DiscountPercentage = 30;
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(It.IsAny<Expression<Func<PromotionGame, bool>>>(), true))
+            .ReturnsAsync(existing);
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.Update(It.IsAny<PromotionGame>()));
+
+        _unitOfWorkMock
+            .Setup(u => u.CommitAsync(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Commit failed"));
+
+        _unitOfWorkMock
+            .Setup(u => u.RollbackAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        Func<Task> act = async () => await _promotionService.UpdatePromotionGameAsync(id, model);
+
+        await act.Should().ThrowAsync<Exception>().WithMessage("Commit failed");
+
+        _unitOfWorkMock.Verify(u => u.RollbackAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeletePromotionGame_PromotionGameNotFound()
+    {
+        var id = Guid.NewGuid();
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<PromotionGame, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<PromotionGame, object>>[]>()
+            ))
+            .ReturnsAsync((PromotionGame)null);
+
+        var result = await _promotionService.DeletePromotionGameAsync(id);
+
+        result.Should().BeNull();
+
+        _promotionGameRepositoryMock.Verify(p => p.Delete(It.IsAny<PromotionGame>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task DeletePromotionGame_WithTransactions()
+    {
+        var promotionGame = new PromotionGame();
+        promotionGame.WalletTransactions.Add(new WalletTransaction());
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<PromotionGame, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<PromotionGame, object>>[]>()))
+            .ReturnsAsync(promotionGame);
+
+        var result = await _promotionService.DeletePromotionGameAsync(promotionGame.Id);
+
+        result.Should().BeFalse();
+
+        _promotionGameRepositoryMock.Verify(p => p.Delete(It.IsAny<PromotionGame>()), Times.Never);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task DeletePromotionGame_Success()
+    {
+        var promotionGame = new PromotionGame(); 
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<PromotionGame, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<PromotionGame, object>>[]>()))
+            .ReturnsAsync(promotionGame);
+
+        _unitOfWorkMock
+            .Setup(u => u.CommitAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+
+        var result = await _promotionService.DeletePromotionGameAsync(promotionGame.Id);
+
+        result.Should().BeTrue();
+
+        _promotionGameRepositoryMock.Verify(p => p.Delete(It.Is<PromotionGame>(pg => pg == promotionGame)), Times.Once);
+        _unitOfWorkMock.Verify(u => u.CommitAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeletePromotionGame_CommitThrowsException()
+    {
+        var promotionGame = new PromotionGame();
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.FirstOrDefaultAsync(
+                It.IsAny<Expression<Func<PromotionGame, bool>>>(),
+                true,
+                It.IsAny<Expression<Func<PromotionGame, object>>[]>()))
+            .ReturnsAsync(promotionGame);
+
+        _promotionGameRepositoryMock
+            .Setup(p => p.Delete(It.IsAny<PromotionGame>()));
+
+        _unitOfWorkMock
+            .Setup(u => u.CommitAsync(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Commit failed"));
+
+        _unitOfWorkMock
+            .Setup(u => u.RollbackAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        Func<Task> act = async () => await _promotionService.DeletePromotionGameAsync(promotionGame.Id);
+
+        await act.Should().ThrowAsync<Exception>().WithMessage("Commit failed");
+
+        _unitOfWorkMock.Verify(u => u.RollbackAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
 }
